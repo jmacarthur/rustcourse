cbec8652b242e73e628cad97de3bc2f3d5cdf9f8
Jim MacArthur <jim@mode7.co.uk>
2024-07-22 21:39:05 +0100

    Add 'bfstrip' initial attempt

---
+++ week1/bfstrip/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs;
+use std::env::args;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let fname = args().nth(1).ok_or("Expected filename")?;
+    let content = fs::read_to_string(fname)?;
+    for c in content.chars() {
+        match c {
+	   '>' => print!("{}", c),
+	   '<' => print!("{}", c),
+	   '+' => print!("{}", c),
+	   '-' => print!("{}", c),
+	   '.' => print!("{}", c),
+	   ',' => print!("{}", c),
+	   '[' => print!("{}", c),
+	   ']' => print!("{}", c),
+	   _ => {}
+	}
+    }
+    print!("\n");
+
+    Ok(())
+}
