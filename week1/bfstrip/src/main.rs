c2280c26b5f546694409b9662de2648648bb62cb
Jim MacArthur <jim.macarthur@codethink.co.uk>
2024-07-23 12:30:44 +0100

    Cargo fmt for bfstrip

--- week1/bfstrip/src/main.rs
+++ week1/bfstrip/src/main.rs
@@ -1,22 +1,22 @@
-use std::fs;
 use std::env::args;
 use std::error::Error;
+use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let fname = args().nth(1).ok_or("Expected filename")?;
     let content = fs::read_to_string(fname)?;
     for c in content.chars() {
         match c {
-	   '>' => print!("{}", c),
-	   '<' => print!("{}", c),
-	   '+' => print!("{}", c),
-	   '-' => print!("{}", c),
-	   '.' => print!("{}", c),
-	   ',' => print!("{}", c),
-	   '[' => print!("{}", c),
-	   ']' => print!("{}", c),
-	   _ => {}
-	}
+            '>' => print!("{}", c),
+            '<' => print!("{}", c),
+            '+' => print!("{}", c),
+            '-' => print!("{}", c),
+            '.' => print!("{}", c),
+            ',' => print!("{}", c),
+            '[' => print!("{}", c),
+            ']' => print!("{}", c),
+            _ => {}
+        }
     }
     print!("\n");
 
