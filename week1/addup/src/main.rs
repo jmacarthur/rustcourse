42330e7f14484d5fd6fccc88855d32c225f396f9
Jim MacArthur <jim.macarthur@codethink.co.uk>
2024-07-23 17:06:33 +0100

    Fix formatting in addup

--- week1/addup/src/main.rs
+++ week1/addup/src/main.rs
@@ -1,20 +1,20 @@
-use std::io::BufReader;
-use std::io::prelude::*;
-use std::fs::File;
 use std::env::args;
 use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let fname = args().nth(1).ok_or("Expected filename")?;
     let f = File::open(fname)?;
     let f_reader = BufReader::new(f);
 
-    let mut total : i32 = 0;
+    let mut total: i32 = 0;
 
     for line in f_reader.lines() {
-	let num : i32 = line.unwrap().trim().parse()?;
-	println!("{}", num);
-	total += num;
+        let num: i32 = line.unwrap().trim().parse()?;
+        println!("{}", num);
+        total += num;
     }
 
     println!("The total is {total}");
