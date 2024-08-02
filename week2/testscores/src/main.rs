42716a5285761595e6c1f090a846648e5e533373
Jim MacArthur <jim.macarthur@codethink.co.uk>
2024-08-02 16:39:02 +0100

    testscores: Read in file and dump it

--- week2/testscores/src/main.rs
+++ week2/testscores/src/main.rs
@@ -1,6 +1,9 @@
 use std::env::args;
 use std::error::Error;
-use std::fs;
+use std::io::BufReader;
+use std::io::BufRead;
+
+use std::fs::File;
 
 enum TestRecord {
     TupleKind(String, i64),
@@ -11,7 +14,11 @@ fn main() -> Result<(), Box<dyn Error>>
 {
     println!("Hello, world!");
     let fname = args().nth(1).ok_or("Expected filename")?;
-
+    let f = File::open(fname)?;
+    let f_reader = BufReader::new(f);
+    for line in f_reader.lines() {
+        println!("{}", line?);
+    }
     Ok(())
 
 }
