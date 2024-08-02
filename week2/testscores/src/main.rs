ae3f6e21d00d1a4a103669ae376749941e835876
Jim MacArthur <jim.macarthur@codethink.co.uk>
2024-08-02 16:34:00 +0100

    TestRecord enum, argument checks

--- week2/testscores/src/main.rs
+++ week2/testscores/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+use std::env::args;
+use std::error::Error;
+use std::fs;
+
+enum TestRecord {
+    TupleKind(String, i64),
+    String
+}
+
+fn main() -> Result<(), Box<dyn Error>>
+{
     println!("Hello, world!");
+    let fname = args().nth(1).ok_or("Expected filename")?;
+
+    Ok(())
+
 }
