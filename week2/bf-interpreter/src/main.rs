70291e2aae9956dcd0015768533f5569c83d87a4
Jim MacArthur <jim.macarthur@codethink.co.uk>
2024-08-06 18:03:20 +0100

    bf-interpreter: Add RawInstruction enum and from_char

--- week2/bf-interpreter/src/main.rs
+++ week2/bf-interpreter/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+enum RawInstruction {
+    IncPointer,
+    DecPointer,
+    IncByte,
+    DecByte,
+    Output,
+    Input,
+    JumpForward,
+    JumpBackward,
 }
+
+impl RawInstruction {
+    pub fn from_char(c: char) -> Option<Self> {
+        match c {
+            '>' => Some(Self::IncPointer),
+            '<' => Some(Self::DecPointer),
+            '+' => Some(Self::IncByte),
+            '-' => Some(Self::DecByte),
+            '.' => Some(Self::Output),
+            ',' => Some(Self::Input),
+            '[' => Some(Self::JumpForward),
+            ']' => Some(Self::JumpBackward),
+            _ => None,
+        }
+    }
+}
+
+fn main() {}
