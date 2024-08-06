fb469222dd00972894f7b7b17dc961696b827756
Jim MacArthur <jim.macarthur@codethink.co.uk>
2024-08-06 16:30:38 +0100

    Cargo fmt

--- week2/testscores/src/main.rs
+++ week2/testscores/src/main.rs
@@ -1,35 +1,34 @@
 use std::env::args;
 use std::error::Error;
-use std::io::BufReader;
 use std::io::BufRead;
+use std::io::BufReader;
 
 use std::fs::File;
 
-#[derive (Debug)]
+#[derive(Debug)]
 enum TestRecord {
     NameAndNumber(String, i64),
-    NameOnly(String)
+    NameOnly(String),
 }
 
 impl TryFrom<&str> for TestRecord {
     type Error = Box<dyn std::error::Error>;
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let parts : Vec<&str> = value.split(":").collect();
+        let parts: Vec<&str> = value.split(":").collect();
         let record = match parts.len() {
             1 => Self::NameOnly(String::from(parts[0])),
             2 => {
-                let score : i64 = parts[1].parse()?;
+                let score: i64 = parts[1].parse()?;
                 Self::NameAndNumber(String::from(parts[0]), score)
-            },
-            _ => panic!("Unimplemented")
+            }
+            _ => panic!("Unimplemented"),
         };
         Ok(record)
     }
 }
 
 fn read_records(filename: &str) -> Result<Vec<TestRecord>, Box<dyn std::error::Error>> {
-
-    let mut records : Vec<TestRecord> = Vec::new();
+    let mut records: Vec<TestRecord> = Vec::new();
     let f = File::open(filename)?;
     let f_reader = BufReader::new(f);
     for line in f_reader.lines() {
@@ -37,21 +36,22 @@ fn read_records(filename: &str) -> Result<Vec<TestRecord>, Box<dyn std::error::E
             Ok(l) => {
                 let record = TestRecord::try_from(&*l);
                 records.push(record?);
-            },
-            Err(x) => { panic!("Failed to read line: {}", x) }
+            }
+            Err(x) => {
+                panic!("Failed to read line: {}", x)
+            }
         }
     }
     Ok(records)
 }
 
-fn main() -> Result<(), Box<dyn Error>>
-{
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
     let fname = args().nth(1).ok_or("Expected filename")?;
     let fname_str = fname.as_str();
     let records = read_records(&fname_str);
     for record in records? {
-        println!("{:?}", record );
+        println!("{:?}", record);
     }
     Ok(())
 }
