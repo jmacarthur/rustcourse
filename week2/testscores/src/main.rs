6e3bbc50c3c8fb06ac8a189b25bcaeffa2cecdf5
Jim MacArthur <jim.macarthur@codethink.co.uk>
2024-08-06 14:21:09 +0100

    Use proper error type in try_from; use read_records

--- week2/testscores/src/main.rs
+++ week2/testscores/src/main.rs
@@ -19,17 +19,17 @@ impl TestRecord {
 
 impl TryFrom<&str> for TestRecord {
     type Error = ();
-    fn try_from(value: &str) -> Result<TestRecord, ()> {
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
         let parts : Vec<&str> = value.split(":").collect();
         let record = match parts.len() {
-            1 => TestRecord::NameOnly(String::from(parts[0])),
+            1 => Self::NameOnly(String::from(parts[0])),
             2 => {
                 // Why can't we just use the '?' operator here? Does it not return error?
                 let score : i64 = match parts[1].parse() {
                     Ok(x) => x,
                     Err(x) => {panic!("{}", x)}
                 };
-                TestRecord::NameAndNumber(String::from(parts[0]), score)
+                Self::NameAndNumber(String::from(parts[0]), score)
             },
             _ => panic!("Unimplemented")
         };
@@ -37,7 +37,7 @@ impl TryFrom<&str> for TestRecord {
     }
 }
 
-fn readRecords(filename: &str) -> Result<Vec<TestRecord>, Box<dyn std::error::Error>> {
+fn read_records(filename: &str) -> Result<Vec<TestRecord>, Box<dyn std::error::Error>> {
 
     let mut records : Vec<TestRecord> = Vec::new();
     let f = File::open(filename)?;
@@ -62,16 +62,9 @@ fn main() -> Result<(), Box<dyn Error>>
 {
     println!("Hello, world!");
     let fname = args().nth(1).ok_or("Expected filename")?;
-    let f = File::open(fname)?;
+    let fname_str = fname.as_str();
+    let f = File::open(fname_str)?;
     let f_reader = BufReader::new(f);
-    for line in f_reader.lines() {
-        match line {
-            Ok(l) => {
-                let record = TestRecord::try_from(&*l);
-                println!("{:?}", record );
-            },
-            Err(x) => { panic!("Failed to read line: {}", x) }
-        }
-    }
+    let records = read_records(&fname_str);
     Ok(())
 }
