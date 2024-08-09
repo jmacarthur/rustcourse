c85e1cd3ea5f7c127e636394e8f32de2260e15c4
Jim MacArthur <jim.macarthur@codethink.co.uk>
2024-08-09 15:31:57 +0100

    bf-interpreter: Add parse_source_file, Source_instruction, file handling ops

--- week2/bf-interpreter/src/main.rs
+++ week2/bf-interpreter/src/main.rs
@@ -1,3 +1,10 @@
+use std::env::args;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+
 enum RawInstruction {
     IncPointer,
     DecPointer,
@@ -25,4 +32,29 @@ impl RawInstruction {
     }
 }
 
-fn main() {}
+struct SourceInstruction {
+    instruction : RawInstruction,
+    col : usize,
+    row : usize
+}
+
+
+fn parse_source_file(filename: &str) -> Result<Vec<SourceInstruction>, Box<dyn std::error::Error>> {
+    let f = File::open(filename)?;
+    let f_reader = BufReader::new(f);
+    let mut line_number : usize = 0;
+    let mut instructions : Vec<SourceInstruction> = Vec::new();
+    for line in f_reader.lines() {
+        line_number += 1;
+    }
+    println!("{} lines processed", line_number);
+    Ok(instructions)
+}
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let fname = args().nth(1).ok_or("Expected filename")?;
+    let fname_str = fname.as_str();
+    let source : Vec<SourceInstruction> = parse_source_file(&fname_str)?;
+    Ok(())
+}
